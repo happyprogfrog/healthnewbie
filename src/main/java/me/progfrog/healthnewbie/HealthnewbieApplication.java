d84cee2e30c94d5557dd7af475259ea7a31f83d0
happyprogfrog <happyprogfrog@gmail.com>
2024-06-10 21:42:20 +0900

    프로젝트 생성

---
+++ src/main/java/me/progfrog/healthnewbie/HealthnewbieApplication.java
@@ -0,0 +1,13 @@
+package me.progfrog.healthnewbie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthnewbieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthnewbieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/progfrog/healthnewbie/HealthnewbieApplicationTests.java
@@ -0,0 +1,13 @@
+package me.progfrog.healthnewbie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthnewbieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
