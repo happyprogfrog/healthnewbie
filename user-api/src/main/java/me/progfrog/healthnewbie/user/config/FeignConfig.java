103c57ea7ad3226bdf086744784220397998197b
happyprogfrog <happyprogfrog@gmail.com>
2024-06-11 12:55:44 +0900

    fix: @EnableFeignClients 애노테이션 선언 위치 변경

--- user-api/src/main/java/me/progfrog/healthnewbie/user/config/FeignConfig.java
+++ user-api/src/main/java/me/progfrog/healthnewbie/user/config/FeignConfig.java
@@ -3,12 +3,10 @@ package me.progfrog.healthnewbie.user.config;
 import feign.auth.BasicAuthRequestInterceptor;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableFeignClients
 public class FeignConfig {
 
     @Value(value = "${mailgun.key}")
